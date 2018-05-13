bbbacaf1e43992594d30bada7cb8f2e47d64241f
wisteria <wisteria0x0f@users.noreply.github.com>
2018-05-13 14:48:52 +0900

    ・文字修飾（一部） ・リファクタリング ・並列化

--- SlackBackup/Program.cs
+++ SlackBackup/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
@@ -11,6 +10,31 @@ namespace SlackBackup
 {
     class Program
     {
+        /// <summary>
+        /// 太字にする部分を検出する正規表現
+        /// </summary>
+        static Regex regexBold = new Regex(@"(?>(^\*|\x20\*)(?<str>\w+?.*?)(\*\x20|\*$))", RegexOptions.Compiled | RegexOptions.Multiline);
+
+        /// <summary>
+        /// 消線にする部分を検出する正規表現
+        /// </summary>
+        static Regex regexLine = new Regex(@"(?>(^~|\x20~)(?<str>\w+?.*?)(~\x20|~$)|(^~|\x20~ )(?<str>\w+?.*)(~\x20|~$))", RegexOptions.Compiled | RegexOptions.Multiline);
+
+        /// <summary>
+        /// 斜体にする部分を検出する正規表現
+        /// </summary>
+        static Regex regexOblique = new Regex(@"(?>(^_|\x20_)(?<str>\w+?.*?)(_\x20|_$)|(^_|\x20_ )(?<str>\w+?.*)(_\x20|_$))", RegexOptions.Compiled | RegexOptions.Multiline);
+
+        /// <summary>
+        /// 添付ファイルURLを検出する正規表現
+        /// </summary>
+        static Regex regexAttachment = new Regex(@"(?><https:\/\/amusementcreators.slack.com\/files\/(?<url>.+)\|(?<filename>.+)>)", RegexOptions.Compiled | RegexOptions.Multiline);
+
+        /// <summary>
+        /// URLを検出する正規表現
+        /// </summary>
+        static Regex regexURL = new Regex(@"(?><http(?<url>.+)>)", RegexOptions.Compiled | RegexOptions.Multiline);
+
         static void Main(string[] args)
         {
             // ユーザデータの読み込み
@@ -22,25 +46,85 @@ namespace SlackBackup
             var channels = JsonConvert.DeserializeObject<Channel[]>(str).ToDictionary(u => u.Id);
 
             // チャンネルごとに、メッセージデータを読み込む
-            foreach (var chName in channels.Select(ch => ch.Value.Name))
-            {
-                var path = Path.Combine(args[0], chName);
-                var files = Directory.EnumerateFiles(path);
-                var messages = new List<Message>();
-
-                // メッセージは日付ごとに別々のファイルになっているので全部読み込む
-                foreach (var f in files)
+            Parallel.ForEach(channels.Select(ch => ch.Value.Name), chName =>
                 {
-                    str = System.IO.File.ReadAllText(f);
-                    var m = JsonConvert.DeserializeObject<Message[]>(str);
-                    Console.WriteLine($"{ f} Loaded...");
-                    messages.AddRange(m);
-                }
+                    var path = Path.Combine(args[0], chName);
+                    var files = Directory.EnumerateFiles(path);
+                    var messages = new List<Message>();
 
-                // チャンネルごとにHTMLファイルに書き出し
-                using (var writer = new StreamWriter(Path.Combine(args[1], chName + ".html")))
-                {
-                    writer.WriteLine($@"
+                    // メッセージは日付ごとに別々のファイルになっているので全部読み込む
+                    foreach (var f in files)
+                    {
+                        str = System.IO.File.ReadAllText(f);
+                        var m = JsonConvert.DeserializeObject<Message[]>(str);
+                        Console.WriteLine($"{ f} Loaded...");
+                        messages.AddRange(m);
+                    }
+
+                    // チャンネルごとにHTMLファイルに書き出し
+                    using (var writer = new StreamWriter(Path.Combine(args[1], chName + ".html")))
+                    {
+                        WriteHeader(writer, chName);
+                        WriteSidebar(writer, channels, chName);
+
+                        foreach (var m in messages)
+                        {
+                            var user = (m.User != null && users.ContainsKey(m.User)) ? users[m.User] : null;
+                            var uname = user?.Name ?? "unknown";
+                            var unamereal = user?.RealName ?? uname;
+                            var date = DateTimeOffset.FromUnixTimeSeconds((long)double.Parse(m.Ts)).ToLocalTime().ToString();
+                            var text = m.Text ?? "";
+
+                            // replace channel id to channel name
+                            foreach (var c in channels)
+                            {
+                                var r = new Regex($@"<#{c.Key}\|?.*>");
+                                text = r.Replace(text, $"<a href=\"{c.Value.Name}.html\">#{c.Value.Name}</a>");
+                            }
+
+                            // replace user id to user name
+                            foreach (var u in users)
+                            {
+                                var r = new Regex($@"<@{u.Key}>");
+                                text = r.Replace(text, $"@{u.Value.Name}({u.Value.RealName ?? u.Value.Name})");
+                            }
+
+                            // replace file url
+                            if (text.Contains(@"<https:\/\/amusementcreators.slack.com\/files\/"))
+                            {
+                                var match = regexAttachment.Match(text);
+                                var url = Regex.Replace($"{match.Groups["url"]}" ?? "", @"\/", @"/");
+                                text = regexAttachment.Replace(text, $"<a href=\"https://amusementcreators.slack.com/files/{url}\">{match.Groups["filename"]}</a>");
+                            }
+
+                            // replace url
+                            if (text.Contains(@"http"))
+                            {
+                                var match = regexURL.Match(text);
+                                var url = Regex.Replace($"{match.Groups["url"]}" ?? "", @"\/", @"/");
+                                text = regexURL.Replace(text, $"<a href=\"http{url}\">http{url}</a>");
+                            }
+
+                            // 文字修飾を反映
+                            text = regexBold.Replace(text, @"<span class='bold'>${str}</span>");
+                            text = regexOblique.Replace(text, @"<span class='oblique'>${str}</span>");
+                            text = regexLine.Replace(text, @"<span class='line'>${str}</span>");
+
+                            WriteMessage(writer, user, uname, unamereal, date, text);
+                        }
+                        WriteFooter(writer);
+                    }
+                });
+            return;
+
+        }
+
+        /// <summary>
+        /// HTMLヘッダを書き出す
+        /// </summary>
+        private static void WriteHeader(StreamWriter writer, string chName)
+        {
+            writer.WriteLine($@"
 <!DOCTYPE html>
 <html>
 <head>
@@ -48,76 +132,53 @@ namespace SlackBackup
     <title>{chName}</title>
     <link rel='stylesheet' type='text/css' href='style.css' />
 </head>
-<body>
-    
+<body>    
 
 ");
-					writer.WriteLine(@"<div class='side_bar'>");
-					foreach(var c in channels)
-					{
-						var text = $"<a href =\"{c.Value.Name}.html\">#{c.Value.Name}</a>\n";
+        }
 
-						if (chName == c.Value.Name)
-						{
-							writer.WriteLine($@"
+        /// <summary>
+        /// サイドバーを書き出す
+        /// </summary>
+        private static void WriteSidebar(StreamWriter writer, Dictionary<string, Channel> channels, string chName)
+        {
+            writer.WriteLine(@"<div class='side_bar'>");
+            foreach (var c in channels)
+            {
+                var text = $"<a href =\"{c.Value.Name}.html\">#{c.Value.Name}</a>\n";
+
+                if (chName == c.Value.Name)
+                {
+                    writer.WriteLine($@"
     <span class='container' id='current'>
         {text}
     </span>
 ");
-						}
-						else
-						{
-							writer.WriteLine($@"
+                }
+                else
+                {
+                    writer.WriteLine($@"
     <span class='container'>
         {text}
     </span>
 ");
-						}
-					}
-					writer.WriteLine("</div>");
-					
-                    foreach (var m in messages)
-                    {
-                        var user = (m.User != null && users.ContainsKey(m.User)) ? users[m.User] : null;
-                        var uname = user?.Name ?? "unknown";
-						var unamereal = user?.RealName ?? uname;
-                        var date = DateTimeOffset.FromUnixTimeSeconds((long)double.Parse(m.Ts)).ToLocalTime().ToString();
-                        var text = m.Text??"";
+                }
+            }
+            writer.WriteLine("</div>");
+        }
 
-                        // replace channel id to channel name
-						foreach(var c in channels)
-						{
-							var r = new Regex($@"<#{c.Key}\|?.*>");
-							text = r.Replace(text, $"<a href=\"{c.Value.Name}.html\">#{c.Value.Name}</a>");
-						}
-
-						// replace user id to user name
-						foreach (var u in users)
-                        {
-                            var r = new Regex($@"<@{u.Key}>");
-							text = r.Replace(text, $"{u.Value.RealName ?? u.Value.Name}(@{u.Value.Name})");
-                        }
-
-                        // replace file url
-						if (text.Contains(@"<https:\/\/amusementcreators.slack.com\/files\/"))
-                        {
-                            var r = new Regex(@"<https:\/\/amusementcreators.slack.com\/files\/(?<url>.+)\|(?<filename>.+)>");
-                            var match = r.Match(text);
-                            var url = Regex.Replace($"{match.Groups["url"]}" ?? "", @"\/", @"/");
-                            text = r.Replace(text, $"<a href=\"https://amusementcreators.slack.com/files/{url}\">{match.Groups["filename"]}</a>");
-                        }
-
-                        // replace url
-						if (text.Contains(@"http"))
-                        {
-                            var r = new Regex(@"<http(?<url>.+)>");
-                            var match = r.Match(text);
-                            var url = Regex.Replace($"{match.Groups["url"]}" ?? "", @"\/", @"/");
-							text = r.Replace(text, $"<a href=\"http{url}\">http{url}</a>");
-                        }
-
-
-                        writer.WriteLine($@"
+        /// <summary>
+        /// メッセージを書き出す
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="user"></param>
+        /// <param name="uname"></param>
+        /// <param name="unamereal"></param>
+        /// <param name="date"></param>
+        /// <param name="text"></param>
+        private static void WriteMessage(StreamWriter writer, User user, string uname, string unamereal, string date, string text)
+        {
+            writer.WriteLine($@"
 <div class='message'>
     <span class='avater'>
         <img src='{user?.Profile?.Image48 ?? ""}' />
@@ -125,7 +186,7 @@ namespace SlackBackup
     <span class='container'>
         <span class='header'>
             <span class='name'>
-                {unamereal} (@{uname})
+                @{uname} ({unamereal})
             </span>
             <span class='date'>
                 {date}
@@ -136,17 +197,19 @@ namespace SlackBackup
         </span>
     </span>
 </div>
-                        ");
-                    }
+");
+        }
 
-                    writer.WriteLine(@"
+        /// <summary>
+        /// HTMLフッタを書き出す
+        /// </summary>
+        /// <param name="writer"></param>
+        private static void WriteFooter(StreamWriter writer)
+        {
+            writer.WriteLine(@"
 </body>
 </html>
-                    ");
-                }
-            }
-            return;
-
+");
         }
     }
 }
